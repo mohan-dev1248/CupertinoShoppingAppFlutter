a234b041d167feef61ff5f015826864fa9bb71c9
Mohan-eng-mtap <mohanakrishna.v@mtap.in>
2021-06-02 00:43:14 +0530

    Added producer consumer pattern Implemented search and product list

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,6 @@
+import 'package:cupertino_shopping/product_list_tab.dart';
+import 'package:cupertino_shopping/search_tab.dart';
+import 'package:cupertino_shopping/shopping_cart_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
@@ -20,11 +23,50 @@ class CupertinoStoreApp extends StatelessWidget{
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: SizedBox(),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:cupertino_shopping/app.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
 
 void main() {
-  runApp(CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(            // NEW
+      create: (_) => AppStateModel()..loadProducts(), // NEW
+      child: CupertinoStoreApp(),                     // NEW
+    ),
+  );
 }
